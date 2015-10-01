d9e3166f4b13d3b7f64d69ba61782c0d34bcf3f5
Massimo Cannavo <canna002@cougars.csusm.edu>
2015-09-30 23:29:44 -0700

    updated main to reflect the loginDialog

--- InventoryManagement/main.cpp
+++ InventoryManagement/main.cpp
@@ -1,4 +1,5 @@
 #include "homepage.h"
+#include "loginDialog"
 #include <QApplication>
 
 int main(int argc, char *argv[])
@@ -6,6 +7,9 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     HomePage w;
     w.show();
+
+    LoginDialog d;
+    d.show();
 
     return a.exec();
 }
